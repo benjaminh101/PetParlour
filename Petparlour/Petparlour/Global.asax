f06dbadeac19c13f78f2b82758628c0187929ef1
Benjamin <Benhdeveloper@gmail.com>
2018-02-17 13:19:34 +0200

    Commit

---
+++ Petparlour/Petparlour/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Petparlour.MvcApplication" Language="C#" %>
---
+++ Petparlour/Petparlour/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Petparlour.Startup))]
+namespace Petparlour
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
